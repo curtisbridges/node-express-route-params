7710f6b3049c0c6d1cddde808e9fdaa299d8469b
Curtis Bridges <curtis@curtisbridges.com>
2020-03-18 13:14:29 -0400

    testing route params
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ app.get('/', (req, res) => {
     res.json({hostname: req.hostname})
 })
 
+app.get('/:one/:two', (req, res) => {
+    res.json({
+        one: req.params.one,
+        two: req.params.two
+    })
+})
+
 app.listen(3000, () => {
     console.log('HTTP Server running on localhost:3000')
 })
